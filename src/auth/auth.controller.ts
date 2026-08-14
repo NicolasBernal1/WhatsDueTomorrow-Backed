4013fb46aa16685e132039198df2996edddb0b38
nicolas <nbernalmunera2005@gmail.com>
2026-08-13 19:59:34 -0500

    Agregados endpoints y logica para verificacion y cambio de contraseña

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { BaseResponseDto } from 'src/common/dtos/base-response.dto';
 import { UserDto } from 'src/common/dtos/user.dto';
 import { LoggedInDto } from './dtos/logged-in.dto';
 import { LoginDto } from './dtos/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { ChangePasswordDto } from './dtos/change-password.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,17 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto): Promise<BaseResponseDto<LoggedInDto>>{
     return await this.authService.login(loginDto);
+  }
+
+  @Patch('change-password')
+  @UseGuards(AuthGuard('jwt'))
+  async changePassword(@Request() req, @Body() changePasswordDto: ChangePasswordDto): Promise<BaseResponseDto<null>> {
+    return await this.authService.changePassword(req.user.sub, changePasswordDto);
+  }
+
+  @Patch('verify-password')
+  @UseGuards(AuthGuard('jwt'))
+  async verifyPassword(@Request() req, @Body('password') password: string): Promise<BaseResponseDto<null>> {
+    return await this.authService.verifyPassword(req.user.sub, password);
   }
 }
